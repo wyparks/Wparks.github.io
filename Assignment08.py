11b6f3983f1c1ddea4c282f4255b42b7b1c2c84b
wyparks <133831487+wyparks@users.noreply.github.com>
2023-06-21 18:43:21 -0700

    Add files via upload

--- Assignment08.py
+++ Assignment08.py
@@ -11,9 +11,6 @@
 # Data -------------------------------------------------------------------- #
 strFileName = 'products.txt'
 lstOfProductObjects = []
-file_obj = None  # An object that represents a file
-row_dic = {}  # A row of data separated into elements of a dictionary {Product,Price}
-choice_str = ""  # Captures the user option selection
 
 class Product:
     """Stores data about a product:
@@ -30,9 +27,31 @@ class Product:
     pass  # remove after code is added
     # TODO: Add Code for Product class (Constructor, Properties, & Methods)
     # constructor __init__(self, xxxxx)
+
     def __init__(self, product_name, product_price):
-        self.product_name = product_name
-        self.product_price = product_price
+        self._product_name = product_name
+        self._product_price = product_price
+    # --Properties--
+    # Product Name
+    @property
+    def product_name(self):
+        return self._product_name
+
+    @product_name.setter
+    def product_name(self, value):
+        self._product_name = value.title()
+     # Product Price
+    @property
+    def product_price(self):
+       return self._product_price
+
+    @product_price.setter
+    def product_price(self, value):
+        self._product_price = value
+
+    # Methods
+    def __str__(self):
+        return f"{self.product_name},{self.product_price}"
 # Data -------------------------------------------------------------------- #
 
 # Processing  ------------------------------------------------------------- #
@@ -48,39 +67,37 @@ class FileProcessor:
         RRoot,1.1.2030,Created Class
         <Your Name>,<Today's Date>,Modified code to complete assignment 8
     """
-    pass
-     @staticmethod
-    def read_data_from_file(file_name, list_of_rows):
+    @staticmethod
+    def read_data_from_file(file_name, lstOfProductObjects):
         """ Reads data from a file into a list of dictionary rows
 
         :param file_name: (string) with name of file:
-        :param list_of_rows: (list) you want filled with file data:
+        :param lstOfProductObjects: (list) you want filled with file data:
         :return: (list) of dictionary rows
         """
-        list_of_rows.clear()  # clear current data
         global file_obj  # using global file variable across all functions
         file_obj = open(file_name, "r")
         for line in file_obj:
-            task, priority = line.split(",")
-            row = {"Product": task.strip(), "Price": priority.strip()}
-            list_of_rows.append(row)
+            data = line.split(",")
+            row = Product(data[0], data[1])
+            lstOfProductObjects.append(row)
         file_obj.close()
-        return list_of_rows
+        return lstOfProductObjects
     # TODO: Add Code to process data to a file
     @staticmethod
-    def write_data_to_file(file_name, list_of_rows):
+    def write_data_to_file(file_name, lstOfProductObjects):
         """ Writes data from a list of dictionary rows to a File
 
         :param file_name: (string) with name of file:
-        :param list_of_rows: (list) you want filled with file data:
+        :param lstOfProductObjects: (list) you want filled with file data:
         :return: (list) of dictionary rows
         """
         global file_obj  # using global here to be consistent across functions
         file_obj = open(file_name, 'w')  # opening file
-        for i in list_of_rows:
-            file_obj.write(i["Product"] + ',' + i["Price"] + '\n')  # writing list to file
+        for i in lstOfProductObjects:
+            file_obj.write(i.__str__() + '\n')  # writing list to file
         file_obj.close()
-        return list_of_rows
+        return lstOfProductObjects
 
 # Processing  ------------------------------------------------------------- #
 
@@ -91,7 +108,7 @@ class IO:
     methods:
         print_menu_items():
 
-        print_current_list_items(list_of_rows):
+        print_current_list_items(lstOfProductObjects):
 
         input_product_data():
 
@@ -120,65 +137,56 @@ class IO:
         """ Gets the menu choice from a user
         :return: string
         """
-        choice = str(input("Which option would you like to perform? [1 to 4] - ")).strip()
-        print()  # Add an extra line for looks
-        return choice
+        return input("Which option would you like to perform? [1 to 4] - ")
 
-    # TODO: Add code to show the current data from the file to user
     @staticmethod
-    def output_current_tasks_in_list(list_of_rows):
+    def output_current_products_in_list(lstOfProductObjects):
         """ Shows the current Tasks in the list of dictionaries rows
 
-        :param list_of_rows: (list) of rows you want to display
+        :param lstOfProductObjects: (list) of rows you want to display
         :return: nothing
         """
         print()
         print("******* The current products are: *******")
-        for row in list_of_rows:
-            print(row["Product"] + " (" + row["Price"] + ")")
+        for row in lstOfProductObjects:
+            print(row)
         print("*******************************************")
         print()  # Add an extra line for looks
-    # TODO: Add code to get product data from user
     @staticmethod
-    def input_new_task_and_priority():
+    def input_new_product():
         """  Gets task and priority values to be added to the list
 
         :return: (string, string) with task and priority
         """
-        task_new = input('Please enter the product you wish to add: ')  # getting task from user
-        priority_new = input(f'Enter the price of {task_new}: ')  # getting priority
-        return task_new, priority_new
+        product_new = input('Please enter the product you wish to add: ')  # getting task from user
+        price_new = input(f'Enter the price of {product_new}: ')  # getting priority
+        return Product(product_name=product_new, product_price=price_new)
 # Presentation (Input/Output)  -------------------------------------------- #
 
 
 # Main Body of Script  ---------------------------------------------------- #
-# TODO: Add Data Code to the Main body
 # Load data from file into a list of product objects when script starts
-FileProcessor.read_data_from_file(file_name=file_name_str, list_of_rows=table_lst)
+strFileName = 'products.txt'
+lstOfProductObjects = []
+FileProcessor.read_data_from_file(file_name=strFileName, lstOfProductObjects=lstOfProductObjects)
 # Show user a menu of options
 # Get user's menu option choice
     # Show user current data in the list of product objects
     # Let user add data to the list of product objects
     # let user save current data to file and exit program
-while (True):
-    # Step 3 Show current data
-    IO.output_current_tasks_in_list(list_of_rows=table_lst)  # Show current data in the list/table
-    IO.output_menu_tasks()  # Shows menu
+while True:
+    IO.print_menu_items()  # Shows menu
     choice_str = IO.input_menu_choice()  # Get menu option
 
-    # Step 4 - Process user's menu choice
-    if choice_str.strip() == '1':  # Add a new Task
-        task, priority = IO.input_new_task_and_priority()
-        table_lst = Processor.add_data_to_list(task=task, priority=priority, list_of_rows=table_lst)
+    if choice_str.strip() == '1':
+        IO.output_current_products_in_list(lstOfProductObjects)
+        continue
+    elif choice_str.strip() == '2':  # Add a new Task
+        new_product = IO.input_new_product()
+        lstOfProductObjects.append(new_product)
         continue  # to show the menu
-
-    elif choice_str == '2':  # Remove an existing Task
-        task = IO.input_task_to_remove()
-        table_lst = Processor.remove_data_from_list(task=task, list_of_rows=table_lst)
-        continue  # to show the menu
-
     elif choice_str == '3':  # Save Data to File
-        table_lst = Processor .write_data_to_file(file_name=file_name_str, list_of_rows=table_lst)
+        FileProcessor.write_data_to_file(file_name=strFileName, lstOfProductObjects=lstOfProductObjects)
         print("Data Saved!")
         continue  # to show the menu
 
